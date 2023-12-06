aef46bd428d14d3ee808b1427c9ce7fda16c56bf
Bardjuki <ahmadbardjukis@gmail.com>
2023-12-06 09:07:32 +0700

    first commit

---
+++ app/users/page.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+interface Users {
+  id: number;
+  name: string;
+  email: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: Users[] = await res.json();
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UsersPage;
